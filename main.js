5eb5625f5ce50480faa54501fc6e902c6c19d40c
Pablo <pablofergon92@gmail.com>
2024-03-12 23:16:28 +0100

    NavBar

---
+++ main.js
@@ -0,0 +1,19 @@
+// Aquí abre en la rueda el 'settings' de la parte de Adriana
+document.getElementById("engranaje").addEventListener("click", function() {
+    const openSettings = document.querySelector("settings")
+    openSettings.style.display = (openSettings.style.display === "none") ? "block" : "none";
+})
+
+
+//Este coódigo abre un div en la barra de búsqueda
+(function() {
+    ('#searchIcon').click(function() {
+        ('#searchLinks').toggle();
+});
+
+(document).click(function(e) {
+    if (!(e.target).closest('#searchBox, #searchIcon').length) {
+        ('#searchLinks').hide();
+}
+});
+});
